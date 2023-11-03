0e50be432671bec004103329d2760d152590b3fa
GowthamB <gowtham.k@cbotsolution.com>
2023-11-03 10:13:50 +0530

    login component changes

--- src/app/_core/auth/login/login.component.ts
+++ src/app/_core/auth/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, Validators } from '@angular/forms';
+import { CommonService } from '@shared/services/common/common.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+
+  loginForm!: FormGroup;
+  formSubmitted = false;
+
+  constructor(private service: CommonService) { }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    
+    this.loadForm();
+
+  }
+
+  // Initiate login form
+
+  loadForm() {
+
+    this.formSubmitted = false;
+
+    this.loginForm = this.service.fb.group({
+
+      'email': ['', [Validators.required, Validators.email]],
+
+      'password': ['',[Validators.required]]
+
+    });
+
+  }
+
+  // convenience getter for easy access to form fields
+
+  get f(): any { return this.loginForm.controls }
 
 }
