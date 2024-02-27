6a85c05f903cda0b954e71c51d4ac7f1e6d0d80a
GowthamB <gowtham.k@cbotsolution.com>
2024-02-27 07:48:23 +0530

    order changes

--- src/app/pages/orders/orders.component.ts
+++ src/app/pages/orders/orders.component.ts
@@ -3,7 +3,6 @@ import { OffcanvasComponent } from '@shared/components';
 import { CommonService } from '@shared/services/common/common.service';
 import * as _ from 'lodash';
 
-
 @Component({
   selector: 'app-orders',
   templateUrl: './orders.component.html',
@@ -19,6 +18,10 @@ export class OrdersComponent {
   totalCount: number = 0;
   tableColumns = ['ORDER NO', 'ORDER DATE', 'CUSTOMER NAME', 'MOBILE', 'MAIL','NOOFITEMS' ,'BOOKEDVIA','ORDER STATUS','PAYMENT STATUS','ACTION'];
   _: any = _;
+  searchValue: string = "";
+  pageSize: number = 10;
+  pageIndex: number = 0;
+  filterValues: any = null;
 
   constructor(public service: CommonService, ){}
 
@@ -26,16 +29,14 @@ export class OrdersComponent {
 
     this.service.setApiLoaders({ "isLoading": true, "url": ["/agent/orders"] });
 
-    this.getOrderList();
-
     this.loadForm();
-    
+
   }
 
   loadForm() {
 
     this.filterForm = this.service.fb.group({
-
+      
       "orderType": [""],
       "fromDate": [""],
       "toDate": [""],
@@ -45,11 +46,37 @@ export class OrdersComponent {
 
     });
 
+    this.filterData();
+
   }
 
-  getOrderList() {
+  filterData() {
 
-    this.service.postService({ url: "/agent/orders" }).subscribe((res: any) => {
+    if(_.isEqual(this.filterValues, _.pickBy(this.filterForm.value))) return this.canvas?.close();
+
+    this.filterValues = _.pickBy(this.filterForm.value);
+
+    this.pageIndex = 0;
+
+    this.pageSize = 10;
+
+    this.getOrderList({});
+
+  }
+
+  getOrderList({ isPageChange = false }: { isPageChange?: Boolean}) {
+
+    if(!isPageChange) {
+
+      this.pageIndex = 0;
+
+      this.pageSize = 10;
+
+    }
+
+    let params = { "pageIndex": this.pageIndex, "pageSize": this.pageSize, "searchValue": this.searchValue };
+
+    this.service.postService({ url: "/agent/orders", "params": params, "payload": _.pickBy(this.filterForm.value) }).subscribe((res: any) => {
       
       if (res.status == "ok") {
 
@@ -58,8 +85,12 @@ export class OrdersComponent {
         this.totalCount = res.totalCount;
 
       }
+
+      this.canvas?.close();
       
     },(error: any) => {
+
+      this.canvas?.close();
       
       this.service.showToastr({ data: { type: "error", message: error?.error?.message || "Data fetching failed" } });
 
