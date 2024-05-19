f75411d9751b75069096f42a626ac4fb70786e2f
balukarthi2324 <balukarthi2324@gmail.com>
2024-05-19 16:14:11 +0530

    userprofile

--- src/app/pages/account-setup/company/account-details/account-details.component.ts
+++ src/app/pages/account-setup/company/account-details/account-details.component.ts
@@ -15,6 +15,9 @@ export class AccountDetailsComponent {
   isLoading: boolean = false;
   _: any = _;
   userSubscribe: any;
+  userProfile: any = '';
+  profileImg: any = '';
+
 
 	constructor(public service: CommonService) {
     
@@ -38,9 +41,11 @@ export class AccountDetailsComponent {
 
       'email': [ this.service.userDetails?.email || '', [Validators.required, Validators.email] ],
 
-      'password': [ '', [Validators.required,Validators.minLength(8)] ],
+      'password': [ '', [Validators.minLength(8)] ],  //[Validators.required,] 
 
-      'confirmPassword': [ '', Validators.required ],
+      'confirmPassword': [''],
+
+      'profileImg': [this.service.userDetails?.profileImg ? this.service.getFullImagePath({ 'imgUrl': this.service.userDetails.profileImg}) : '' ],
 
     },{
 
@@ -48,10 +53,67 @@ export class AccountDetailsComponent {
 
     });
 
+    this.accountDetailsFrom.get('password')?.valueChanges.subscribe((value)=>{
+
+      if(value) this.accountDetailsFrom.get('confirmPassword')?.setValidators([Validators.required]);
+
+      else this.accountDetailsFrom.get('confirmPassword')?.clearValidators();
+
+      this.accountDetailsFrom.get('confirmPassword')?.updateValueAndValidity();
+
+    });
+
   }
 
   get f(): any { return this.accountDetailsFrom.controls; }
 
+  uploadFile(event:any) {
+
+    const file = event.target?.files[0]; // Here we use only the first file (single file)
+
+    if(file) {
+
+      const reader = new FileReader();
+
+      // validate file type & size
+
+      const validFileExtensions = ['image/jpeg','image/jpg','image/png'];
+
+      if(!validFileExtensions.includes(file.type)) return this.service.showToastr({ data: { type: "error", message: "Invalid file type" } });
+
+      if(file.size > 1048576) return this.service.showToastr({ data: { type: "error", message: "File size should not exceed 1MB" } });
+
+      // check dimensions of image
+
+      // const img = new Image();
+
+      // img.src = window.URL.createObjectURL(file);
+
+      // img.onload = () => {
+
+      //   if(img.width != 150 || img.height != 150) return this.service.showToastr({ data: { type: "error", message: "Image should be 150px X 150px" } });
+
+      //   else {
+
+          // File Preview
+      
+          reader.onload = () => {
+
+            this.accountDetailsFrom.patchValue({ "profileImg": reader.result as string });
+
+            this.profileImg = file;
+
+          }
+
+          reader.readAsDataURL(file);
+
+      //   }
+
+      // }
+
+    }
+
+  }
   submit() {
 
     this.formSubmitted = true;
@@ -60,9 +122,15 @@ export class AccountDetailsComponent {
 
     this.isLoading = true;
 
-    let payload: any = _.omit(this.accountDetailsFrom.value,['confirmPassword']);
+    const payload = _.omit(this.accountDetailsFrom.getRawValue(),'confirmPassword','profileImg')
 
-    this.service.patchService({ "url": "/me/accountDetails", 'payload': payload }).subscribe((res: any) => {
+    const formData = new FormData();
+
+    formData.append("data", JSON.stringify(payload));
+
+    if(this.profileImg) formData.append("profileImg", this.profileImg);    
+
+    this.service.patchService({ "url": "/me/accountDetails", 'payload': formData }).subscribe((res: any) => {
 
       if(res.status=='ok') {
 
